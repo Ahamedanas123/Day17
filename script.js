03212ad876810948aff312c9afd08bce0b5b977a
Ahamedanas123 <121920106+Ahamedanas123@users.noreply.github.com>
2023-02-09 15:00:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var  count = document.getElementById('count')
+setTimeout(()=>{
+    console.log(10);
+    count.innerText=10;
+    setTimeout(()=>{
+        console.log(9);
+        count.innerText=9;
+        setTimeout(()=>{
+            console.log(8);
+            count.innerText=8;
+            setTimeout(()=>{
+                console.log(7);
+                count.innerText=7;
+                setTimeout(()=>{
+                    console.log(6);
+                    count.innerText=6;
+                    setTimeout(()=>{
+                        console.log(5);
+                        count.innerText=5;
+                        setTimeout(()=>{
+                            console.log(4);
+                            count.innerText=4;
+                            setTimeout(()=>{
+                                console.log(3);
+                                count.innerText=3;
+                                setTimeout(()=>{
+                                    console.log(2);
+                                    count.innerText=2;
+                                    setTimeout(()=>{
+                                        console.log(1);
+                                        count.innerText=1;
+                                        setTimeout(()=>{
+                                             console.log("Happy Independence Day")
+                                             count.innerText="Happy Independence Day";
+                                         },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)                 
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
